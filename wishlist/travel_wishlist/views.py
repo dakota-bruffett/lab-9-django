fe8b5262df83e422e1f69bddf053e90cb869555a
nq2623up <DakotaBruffett@gmail.com>
2023-11-02 14:42:20 -0500

    Adding the about page

--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from.models import Place
 from .forms import NewPlaceForm
 # Create your views here.
 def place_list(request):
+
+    if request.method == 'POST':
+        #This makes a form for the view
+        form = NewPlaceForm(request.POST)
+        place = form.save()
+        if form.is_valid():
+            place.save()
+            return redirect('place_list')
+
+
+
     places = Place.objects.filter(visited=False)
     new_place_form= NewPlaceForm()
     return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
+def about(request):
+    author = 'Dakota'
+    About = 'A website that helps u find places that u want to go to'
+    return render(request, 'travel_wishlist/about.html', {'author': author},{'About': About})
