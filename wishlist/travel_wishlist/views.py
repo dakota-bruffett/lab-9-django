cd0100154f72ffa8b898f4fd77db2195e21fcd65
nq2623up <DakotaBruffett@gmail.com>
2023-11-02 13:28:09 -0500

    First List of places

---
+++ wishlist/travel_wishlist/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from.models import Place
+# Create your views here.
+def place_list(request):
+    places = Place.objects.filter(visited=False)
+    return render(request, 'travel_wishlist/wishlist.html', {'places': places})
