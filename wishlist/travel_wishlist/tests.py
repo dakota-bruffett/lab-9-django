33948cd7a5fa17c2023018bfc70ffe2af39aef78
nq2623up <DakotaBruffett@gmail.com>
2023-11-02 16:57:03 -0500

    unit testing

--- wishlist/travel_wishlist/tests.py
+++ wishlist/travel_wishlist/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from .models import Place
 
 # Create your tests here.
+class TestHomePage(TestCase):
+    def test_load_page_shows_empty_list(self):
+        home_url_page = reverse('place_list')
+        responce = self.client.get(home_url_page)
+        self.assertTemplateUsed(responce, 'travel wishlist/travel_wishlists.html')
+        self.assertContains(responce, 'you have no wishlist')
+class TestWishList(TestCase):
+    fixtures = ['test_Places']
+
+def test_view_wishlist(self):
+    responce = self.client.get(reverse('place_list'))
+    self.assertTemplateUsed(responce, 'travel_wishlist/travel_wishlist.html')
